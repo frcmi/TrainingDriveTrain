9df46ab543acdafacf7eafb19d5788234e5f3ae7
Tong Lin <38441172+GermanShepherd13@users.noreply.github.com>
2022-01-05 17:35:54 -0800

    fixed pneumatics solenoid to not have same value as gearbox solenoid

--- src/main/java/frc/robot/commands/ShiftGear.java
+++ src/main/java/frc/robot/commands/ShiftGear.java
@@ -5,13 +5,14 @@
 package frc.robot.commands;
 import frc.robot.Robot;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot;
 
 public class ShiftGear extends CommandBase {
-  boolean done;
   /** Creates a new ShiftGear. */
   public ShiftGear() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
+  //EncodersRegular encoders = Robot.container.encodersRegular;
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
@@ -19,20 +20,22 @@ public class ShiftGear extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   //When shifting, position is unreliable
   @Override
-  public void execute() {  
+  public void execute() { 
+    if ((Robot.container.drive.getEncoderVelocityLeft() + Robot.container.drive.getEncoderVelocityRight()) /2 > 5.0 &&  ) {
       Robot.container.shiftingGearbox.shift();
-      done = true;
+    } 
+    if ((Robot.container.drive.getEncoderVelocityLeft() + Robot.container.drive.getEncoderVelocityRight()) /2 < 5.0 && Robot.container.shiftingGearbox.getValue()) {
+      Robot.container.shiftingGearbox.shift();
+    } 
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    done = false;
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return done;
+    return false;
   }
 }
